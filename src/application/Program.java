8a29b9f8975349c1935195fdfd55919b3582f415
vitorbnr <vitorbernardotech@gmail.com>
2024-07-03 17:15:31 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,8 @@ import model.dao.SellerDao;
 import model.entities.Seller;
 
 import model.entities.Department;
+
+import java.util.Date;
 import java.util.List;
 
 public class Program {
@@ -30,5 +32,11 @@ public class Program {
         for (Seller obj : list) {
             System.out.println(obj);
         }
+
+        System.out.println("\n=== TEST 4: seller insert =====");
+        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! New id = " + newSeller.getId());
+
     }
 }
